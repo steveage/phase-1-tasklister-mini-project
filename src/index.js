03ccbb49a41a057832c463780f72f840e80b545a
Mariusz <mariuszbia@yahoo.com>
2022-05-02 23:11:57 -0500

    Started assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const tasks = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", processTask);
 });
+
+function processTask(event) {
+  const task = getTask();
+  tasks.push(task);
+  event.preventDefault();
+  console.log("Task processed.");
+}
+
+function getTask() {
+  const taskInput = document.getElementById("new-task-description");
+  const task = taskInput.textContent;
+  return task;
+}
