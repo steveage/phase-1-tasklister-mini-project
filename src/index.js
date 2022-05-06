24b99c189c7f8e5a7b95a7c33d93799ca83055a4
Mariusz <mariuszbia@yahoo.com>
2022-05-05 23:39:49 -0500

    Completed assignment.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const tasks = [];
+const myTasks = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
@@ -7,14 +7,49 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function processTask(event) {
-  const task = getTask();
-  tasks.push(task);
   event.preventDefault();
-  console.log("Task processed.");
+  saveTask();
+  addListItem();
+}
+
+function saveTask() {
+  const task = getTask();
+  myTasks.push(task);
 }
 
 function getTask() {
-  const taskInput = document.getElementById("new-task-description");
-  const task = taskInput.textContent;
+  const taskInput = getInputUI();
+  const task = taskInput.value;
   return task;
 }
+
+function getInputUI() {
+  return document.getElementById("new-task-description");
+}
+
+function addListItem() {
+  const list = getList();
+  const listItem = getNewListItem();
+  list.appendChild(listItem);
+}
+
+const getList = () => document.getElementById("list");
+
+function getNewListItem() { 
+  const task = getLastTask();
+  const listItem = document.createElement("li");
+  const text = document.createTextNode(task);
+  const button = document.createElement("button");
+  button.addEventListener("click", (e) =>{
+    const task = e.target.parentElement.childNodes[0].textContent;
+    myTasks.splice(myTasks.indexOf(task), 1);
+    listItem.remove();
+    e.preventDefault;
+  });
+  button.innerText = "Delete";
+  listItem.appendChild(text);
+  listItem.appendChild(button);
+  return listItem;
+}
+
+const getLastTask = () => myTasks[myTasks.length - 1];
